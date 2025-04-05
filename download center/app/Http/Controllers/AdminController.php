5b6c3dc60ef7637dd85306e6acd8bde46c7b085f
Shayan Moradi <moradis742@gmail.com>
2025-04-05 13:42:17 +0430

    persian jalali

--- download center/app/Http/Controllers/AdminController.php
+++ download center/app/Http/Controllers/AdminController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function AdminProfile()
     {
-        return view('AdminProfile');
+        $user = User::where('role','=','master')->get();
+        return view('AdminProfile' , compact('user'));
+    }
+
+    public function AddStudyField()
+    {
+        return view('AdminViews.AddStudyField');
     }
 }
--- download center/routes/web.php
+++ download center/routes/web.php
@@ -29,6 +29,8 @@ require __DIR__.'/auth.php';
 Route::middleware(['auth', RoleMiddleware::class . ':admin'])
     ->group(function () {
         Route::get('/AdminProfile', [AdminController::class, 'AdminProfile'])->name('AdminProfile');
+        // Route::get('/AdminProfile/AddStudyField', [AdminController::class, 'AddStudyField'])->name('AddStudyField');
+        Route::get('/AddStudyField', [AdminController::class, 'AddStudyField'])->name('AddStudyField');
        
     });
 
